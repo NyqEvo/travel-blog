760f69d17a58f26b65696443b9ccb472023b5047
NyqEvo <117671943+NyqEvo@users.noreply.github.com>
2023-01-18 14:43:34 -0500

    fixed functionality of signup and login pages

--- public/js/login.js
+++ public/js/login.js
@@ -15,7 +15,7 @@ const loginFormHandler = async (event) => {
   
       if (response.ok) {
         // If successful, redirect the browser to the profile page
-        document.location.replace('/home');
+        document.location.replace('/main');
       } else {
         alert(response.statusText);
       }
--- public/js/signup.js
+++ public/js/signup.js
@@ -13,7 +13,7 @@ const signupFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        document.location.replace('/home');
+        document.location.replace('/main');
       } else {
         alert(response.statusText);
       }
