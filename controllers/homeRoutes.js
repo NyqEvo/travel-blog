2196b0894951b0d6945bc4d9d27bae80ab7e0fd9
NyqEvo <117671943+NyqEvo@users.noreply.github.com>
2023-01-19 10:24:10 -0500

    added requests to give data to handlebars for page load

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -8,7 +8,21 @@ const withAuth = require('../utils/auth');
 //render homepage
 router.get('/', async (req, res) => {
     try {
-        res.render('landing')
+        const postdata = await Post.findAll({
+            include: [
+                {
+                    model:User,
+                    attributes: ['name']
+                }
+            ]
+        })
+
+        const posts = postdata.map((post) => post.get({ plain: true }));
+
+        res.render('landing', {
+            ...posts,
+            logged_in: req.session.logged_in
+        });
     } catch (err) {
         res.status(500).json(err);
     }
@@ -20,11 +34,18 @@ router.get('/post/:id', async (req, res) => {
         const postData = await Post.findByPk(req.params.id, {
             include: [
                 {
-                    //data related to posts
+                    model: User,
+                    attributes: ['name']
                 }
             ]
         });
-        res.send('REPLACE ME LATER')
+
+        const post = postData.get({ plain: true })
+
+        res.render('displaypost', {
+            ...post,
+            logged_in: req.session.logged_in
+        })
     } catch (err) {
         res.status(500).json(err);
     }
