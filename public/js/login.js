3a67e72cbafc2a0b4642c8fe4a59446834f8dcfe
NyqEvo <117671943+NyqEvo@users.noreply.github.com>
2023-01-19 12:37:24 -0500

    adjusted layout.handlebars file

--- public/js/login.js
+++ public/js/login.js
@@ -15,7 +15,7 @@ const loginFormHandler = async (event) => {
   
       if (response.ok) {
         // If successful, redirect the browser to the profile page
-        document.location.replace('/main');
+        document.location.replace('/layout');
       } else {
         alert(response.statusText);
       }
