7526d51bfdaf94f871415e85c263c7261e71cfec
Bryan davie <b.davie81@gmail.com>
2023-01-19 23:30:00 -0500

    saving for pull

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -67,7 +67,7 @@ router.get('/post/:id', async (req, res) => {
         const post = postData.get({ plain: true })
 
         res.render('displaypost', {
-            ...post,
+            post,
             logged_in: req.session.logged_in
         })
     } catch (err) {
