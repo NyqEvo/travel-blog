2649290c422ac23b6a469a72f618942301e8c0bd
Bryan davie <b.davie81@gmail.com>
2023-01-20 14:56:25 -0500

    saving uploaded photo id to databases

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,10 +3,12 @@ const postRoutes = require('./Post');
 const userRoutes = require('./User');
 const commentRoutes = require('./Comment');
 const tagRoutes = require('./Tag');
+const cloudinary = require('./cloudinary');
 
 router.use('/post', postRoutes);
 router.use('/user', userRoutes);
 router.use('/comment', commentRoutes);
 router.use('/tag', tagRoutes);
+router.use('/add-cloudinary', cloudinary);
 
 module.exports = router;
