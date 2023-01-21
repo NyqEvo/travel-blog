8b6e92396c9314fd8a687b3ec585ab207c64b6fe
jdettelback <jdettelback@gmail.com>
2023-01-21 16:43:52 -0500

    passport check

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+require("dotenv").config();
+
+var express = require("express");
+var path = require("path");
+var app = express();
+
 var indexRouter = require("./routes/index");
 var authRouter = require("./routes/auth");
 
 app.use("/", indexRouter);
 app.use("/", authRouter);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 
 var session = require("express-session");
 var passport = require("passport");
@@ -10,14 +19,6 @@ var passport = require("passport");
 var SQLiteStore = require("connect-sqlite3")(session);
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use(
-  session({
-    secret: "keyboard cat",
-    resave: false,
-    saveUninitialized: false,
-    store: new SQLiteStore({ db: "sessions.db", dir: "./var/db" }),
-  })
-);
 
 app.use(
   session({
@@ -27,4 +28,7 @@ app.use(
     store: new SQLiteStore({ db: "sessions.db", dir: "./var/db" }),
   })
 );
+
 app.use(passport.authenticate("session"));
+
+module.exports = app;
